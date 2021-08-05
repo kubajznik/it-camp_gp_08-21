c13314a883a513ac3ecdecf57b0a6b09a7eeb803
Jakub <stribrny@campus.tu-berlin.de>
2021-08-05 23:01:55 +0200

    css wiederherstellung

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,5 @@
 let Aussehen = 3;
+let player;
 
 function setPosition(player) {
   player
@@ -34,7 +35,14 @@ function Aussehenaendern() {
 $('#Start_Button').on('click', event => {
   /*Startet das Spiel*/
 });
+
+function setStartPosition() {
+  spielfeld[(20 - 1) * 30 + 15 - 1]
+}
+
 $(document).ready(e => {
+  player = {reihe: 20, spalte: 15}
+
   $('#Player').on('click', event => {
     $('.Charaktere').toggleClass('hide');
     $('#Player').on('click', envent => {
@@ -61,35 +69,35 @@ $(document).ready(e => {
     switch (event.code) {
       case "ArrowDown":
         tmp = $('#Player').css('top');
-        $('#Player').css('top', parseInt(tmp) + 10);
+        $('#Player').css('top', parseInt(tmp) + 40);
         break;
       case "ArrowUp":
         tmp = $('#Player').css('top');
-        $('#Player').css('top', parseInt(tmp) - 10);
+        $('#Player').css('top', parseInt(tmp) - 40);
         break;
       case "ArrowLeft":
         tmp = $('#Player').css('left');
-        $('#Player').css('left', parseInt(tmp) - 10);
+        $('#Player').css('left', parseInt(tmp) - 40);
         break;
       case "ArrowRight":
         tmp = $('#Player').css('left');
-        $('.Player').css('left', parseInt(tmp) + 10);
+        $('.Player').css('left', parseInt(tmp) + 40);
         break;
       case "KeyS":
         tmp = $('#Player').css('top');
-        $('#Player').css('top', parseInt(tmp) + 10);
+        $('#Player').css('top', parseInt(tmp) + 40);
         break;
       case "KeyW":
         tmp = $('#Player').css('top');
-        $('#Player').css('top', parseInt(tmp) - 10);
+        $('#Player').css('top', parseInt(tmp) - 40);
         break;
       case "KeyA":
         tmp = $('#Player').css('left');
-        $('#Player').css('left', parseInt(tmp) - 10);
+        $('#Player').css('left', parseInt(tmp) - 40);
         break;
       case "KeyD":
         tmp = $('#Player').css('left');
-        $('#Player').css('left', parseInt(tmp) + 10);
+        $('#Player').css('left', parseInt(tmp) + 40);
         break;
       default:
         console.log('wrong key');
--- js/spielfeld.js
+++ js/spielfeld.js
@@ -1,34 +1,34 @@
 let level1;
-$(function() {
+$(function () {
     level1 = erzeugeSpielfeld(20, 30);
     zeigeSpielfeld(level1);
 });
 
 function aendereFeld(spielfeld, reihe, spalte, gelaende) {
-    spielfeld[(reihe-1)*20+spalte].gelaende = gelaende;
+    spielfeld[(reihe - 1) * 30 + spalte - 1].gelaende = gelaende;
     //spielfeld.find(arr => {arr.reihe === reihe && arr.spalte === spalte});
-    console.log();
+
     zeigeSpielfeld(spielfeld);
 }
 
 function erzeugeSpielfeld(reihe, spalte) {
     let spielfeld = [];
 
-    for(let i = 1; i <= reihe; i++){
-        for(let u = 1; u <= spalte; u++){
+    for (let i = 1; i <= reihe; i++) {
+        for (let u = 1; u <= spalte; u++) {
             let feld;
-            if((i === 1 && u === 15) || (i === 20 && u === 15)){
+            if ((i === 1 && u === 15) || (i === 20 && u === 15)) {
                 feld = erzeugeFeld("Tuer", i, u);
-            } else if(i === reihe){
+            } else if (i === reihe) {
                 feld = erzeugeFeld("Wand_Unten", i, u);
             } else if (i === 1) {
                 feld = erzeugeFeld("Wand_Oben", i, u);
             } else if (u === spalte) {
-                feld = erzeugeFeld("Wand_Links", i, u);
-            } else if (u === 1) {
                 feld = erzeugeFeld("Wand_Rechts", i, u);
-            } else{
-                feld=erzeugeFeld("Boden", i, u);
+            } else if (u === 1) {
+                feld = erzeugeFeld("Wand_Links", i, u);
+            } else {
+                feld = erzeugeFeld("Boden", i, u);
             }
             spielfeld.push(feld);
         }
@@ -52,13 +52,16 @@ function erzeugeFeld(gelaende, reihe, spalte) {
     }
 }
 
-function zeigeSpielfeld (spielfeld) {
-for(let e of spielfeld) {
- if(e.gelaende === 'Wand_Unten')	$('#spielfeld').append('<div class="wand_unten">');
- if(e.gelaende === 'Wand_Oben')	$('#spielfeld').append('<div class="wand_oben">');
- if(e.gelaende === 'Wand_Rechts')$('#spielfeld').append('<div class="wand_rechts">');
- if(e.gelaende === 'Wand_Links')	$('#spielfeld').append('<div class="wand_links">');
- if(e.gelaende === 'Boden')	    $('#spielfeld').append('<div class="boden">');
- if(e.gelaende === 'Tuer')	    $('#spielfeld').append('<div class="tuer">');
-}
+function zeigeSpielfeld(spielfeld) {
+    $('#spielfeld')
+        .empty()
+        .append('<div id="Player">');
+    for (let e of spielfeld) {
+        if (e.gelaende === 'Wand_Unten') $('#spielfeld').append('<div class="wand_unten">');
+        if (e.gelaende === 'Wand_Oben') $('#spielfeld').append('<div class="wand_oben">');
+        if (e.gelaende === 'Wand_Rechts') $('#spielfeld').append('<div class="wand_rechts">');
+        if (e.gelaende === 'Wand_Links') $('#spielfeld').append('<div class="wand_links">');
+        if (e.gelaende === 'Boden') $('#spielfeld').append('<div class="boden">');
+        if (e.gelaende === 'Tuer') $('#spielfeld').append('<div class="tuer">');
+    }
 }
