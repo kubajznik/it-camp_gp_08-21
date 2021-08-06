6cde50e50d4e1f174ec0503727b49890a5e9e9ad
OskarGER05 <71186734+OskarGER05@users.noreply.github.com>
2021-08-06 12:04:20 +0200

    Funktion fehlt

--- js/interaktion.js
+++ js/interaktion.js
@@ -2,6 +2,6 @@
 function interaktionenLaden() {
 		if (spielerAufFeld($(".Druckplatte"))) {
 			spielfeld[14].kollidiert = false;
-			alert("----Eine Tür hat sich geöffnet.----")
+			alert("----Eine Tür hat sich geöffnet.----");
 		}
 };
