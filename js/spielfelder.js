4d5e92c659d930310fd99d8d2fe6fe2823ed9bc1
Benjamin Berger <benjamin@berger-dresden.de>
2021-08-06 12:13:45 +0200

    Level 4

--- js/spielfelder.js
+++ js/spielfelder.js
@@ -12,7 +12,6 @@ function erzeugeSpielfeld(nummer) {
 
     switch(nummer) {
         case 1:
-            aendereFeld(spielfeld, 11, 15, 'Druckplatte');
             break;
         case 2:
             
@@ -20,8 +19,27 @@ function erzeugeSpielfeld(nummer) {
         case 3:
 
             break;
-        case 4:
-
+        case 4: 
+		aendereFeld(spielfeld,5,2,"wand")
+		aendereFeld(spielfeld,5,3,"wand")
+		aendereFeld(spielfeld,5,4,"wand")
+		aendereFeld(spielfeld,5,5,"wand")
+		aendereFeld(spielfeld,5,6,"wand")
+		aendereFeld(spielfeld,5,7,"wand")
+		aendereFeld(spielfeld,5,8,"wand")
+		aendereFeld(spielfeld,5,9,"wand")
+		aendereFeld(spielfeld,5,10,"wand")
+		aendereFeld(spielfeld,5,11,"wand")
+		aendereFeld(spielfeld,5,12,"wand")
+		aendereFeld(spielfeld,4,12,"wand")
+		aendereFeld(spielfeld,3,12,"wand")
+		aendereFeld(spielfeld,3,13,"wand")
+		aendereFeld(spielfeld,3,14,"wand")
+		aendereFeld(spielfeld,3,15,"wand")
+		aendereFeld(spielfeld,6,16,"wand")
+		aendereFeld(spielfeld,6,17,"wand")
+		aendereFeld(spielfeld,6,18,"wand")
+		aendereFeld(spielfeld,6,19,"wand")
             break;
         case 5:
 
